8160a377ece3b3ec57e0e25d54f2bba6d4665412
Your Name <machlinsekhar@gmail.com>
2022-07-01 23:36:50 +0530

    Contact

---
+++ mahesh/form.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['fname']
+$email = $_POST['email']
+$phone = $_POST['phone']
+$venue = $_POST['venue']
+$message = $_POST['message']
+
+$email_from = 'machlinsekhar@gmail.com'
+$email_subject = 'New Customer'
+$email_body = "User Name: $name.\n".
+              "User Email: $email.\n".
+              "User Phone: $phone.\n".
+              "Venue: $venue.\n".
+              "User Message: $message.\n".;
+    $to = 'machlinjeyasekhar@gmail.com';
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: contact.html");
+?>
